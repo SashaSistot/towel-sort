74c4b0a2aba7df6e4c55c32a0500bdb0b610021c
Sasha Sistot <sistot.mytishchi@yandex.ru>
2023-02-17 01:26:30 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 
 module.exports = function towelSort (matrix) {
   matrix.map((item, index, arr) => {
-    if (index % 2 == 0) item.reverse();
+    if (index % 2 !== 0) item.reverse();
   });
   matrix = matrix.join(',').split(',');
   return matrix.map(item => +item);
